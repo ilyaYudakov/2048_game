babbf74cd06ca566028d6cf00b7cfc6646a0d110
ilya Yudakov <iyudakov@icloud.com>
2021-09-18 00:16:36 +0300

    add tests replace def in file logics.py

--- main.py
+++ main.py
@@ -1,40 +1,4 @@
-import random
-
-def pretty_print(mas):
-    print('-' * 10)
-    for row in mas:
-        print(*row)
-    print('-' * 10)
-
-
-def get_numbers_from_index(i, j):
-    return i * 4 + j + 1
-
-def get_index_from_number(num):
-    num -= 1
-    x, y = num // 4, num % 4
-    return x, y
-
-def insert_2_or_4(mas, x , y):
-    if random.random() <= 0.75:
-        mas[x][y] = 2
-    else:
-        mas[x][y] = 4
-    return mas
-
-
-def get_empty_list(mas):
-    empty = []
-    for i in range(4):
-        for j in range(4):
-            if mas[i][j] == 0:
-                num = get_numbers_from_index(i, j)
-                empty.append(num)
-    return empty
-
-def is_zero_in_pass(mas):
-    pass
-
+from logics import *
 
 mas = [
     [0, 0, 0, 0],
@@ -45,10 +9,10 @@ mas = [
 
 mas[1][2] = 2
 mas[3][0] = 4
-print (get_empty_list(mas))
+print(get_empty_list(mas))
 pretty_print(mas)
 
-while True:
+while is_zero_in_mas(mas):
     input()
     empty = get_empty_list(mas)
     random.shuffle(empty)
@@ -57,4 +21,3 @@ while True:
     mas = insert_2_or_4(mas, x, y)
     print(f'Мы заполнили элемент под номером {random_num}')
     pretty_print(mas)
-
